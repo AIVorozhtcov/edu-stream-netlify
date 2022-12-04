fb963285cbc38686d2d6ab170f9d5937fe3bead1
Ворожцов Артем Ильич <a_vorozhtsov@step.ru>
2022-12-04 05:44:52 +0300

    demo launch 61111

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,6 +74,16 @@ export default {
       threshold: 8192,
     },
   },
+  image: {
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+    }
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
