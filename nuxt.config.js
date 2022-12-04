7c55e029bcf573e839a24825233731495052542d
Ворожцов Артем Ильич <a_vorozhtsov@step.ru>
2022-12-04 08:12:54 +0300

    building netlify forms 81223qw2ds33wsa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
-  ssr: true,
+  ssr: false,
   
   //server: {     
     //port: 8000, // default: 3000     
