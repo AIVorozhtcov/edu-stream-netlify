d7ec6a7c04c76ce6123347c068498b2cca362fb0
Ворожцов Артем Ильич <a_vorozhtsov@step.ru>
2022-12-05 03:08:41 +0300

    building netlify forms 81223qw2ds33wsasdsdjjhsssdkadss

--- components/ModalForm.vue
+++ components/ModalForm.vue
@@ -12,7 +12,7 @@
           </template> 
           <form name="modalContacts"
               method="POST"
-              action="/"
+              action="/?success=true"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
               :class="[this.$store.getters['getIsMobile'] ? 'hidden' : 'flex flex-column items-start']" style="margin-left:7%; margin-right:7%"
